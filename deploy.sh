88deb6952208b0ec4dbc3ba3a5df9f7a622bc7b8
Art-Invis <151573705+Art-Invis@users.noreply.github.com>
2025-10-02 22:25:18 +0300

    Create workflow

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -e
+
+cd /home/ubuntu/nova-post-cloud-db
+
+# оновлюємо код з GitHub
+git fetch --all
+git reset --hard origin/main
+
+# активуємо virtualenv
+source venv/bin/activate
+pip install --upgrade pip
+pip install -r app/nova-post-cloud-db/requirements.txt
+deactivate
+
+# перезапускаємо сервіс
+sudo systemctl daemon-reload
+sudo systemctl restart tracking.service
+
+echo "Deploy finished: $(date)"
